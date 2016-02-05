f28d636203a84618ed03e3797aac95b4ba8f12a6
Mateusz Karwat <mateuszkarwat@gmail.com>
2016-02-05 11:17:53 +0100

    Initial Commit

---
+++ Napi/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Napi
+//
+//  Created by Mateusz Karwat on 05/02/16.
+//  Copyright © 2016 Mateusz Karwat. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+}
+
---
+++ Napi/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Napi
+//
+//  Created by Mateusz Karwat on 05/02/16.
+//  Copyright © 2016 Mateusz Karwat. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+}
+
