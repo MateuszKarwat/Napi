92e904258825149d6b76658fbcac7cf2c588d62c
Mateusz Karwat <mateuszkarwat@gmail.com>
2016-11-30 19:51:03 +0100

    Create 'Unziper' struct to extract archives

---
+++ Napi/Models/Extensions/URL+FileManagerChecks.swift
@@ -0,0 +1,9 @@
+//
+//  URL+FileManagerChecks.swift
+//  Napi
+//
+//  Created by Mateusz Karwat on 30/11/2016.
+//  Copyright © 2016 Mateusz Karwat. All rights reserved.
+//
+
+import Foundation
---
+++ Napi/Models/Unziper.swift
@@ -0,0 +1,39 @@
+//
+//  Created by Mateusz Karwat on 30/11/2016.
+//  Copyright © 2016 Mateusz Karwat. All rights reserved.
+//
+
+import Foundation
+
+struct Unziper {
+
+    /// Extract compressed files in an archive using `unzip` command line.
+    ///
+    /// - Parameters:
+    ///   - fileURL:     A `URL` to the archive.
+    ///   - destination: A directory the archive should be extracted to.
+    ///   - overwrite:   If it's set to `true` all exisiting files will be replaced in case of collision.
+    ///   - password:    A password to the archive.
+    static func unzipFile(at fileURL: URL, destination: URL? = nil, overwrite: Bool = false, password: String? = nil) {
+        guard fileURL.isFile, fileURL.exists else { return }
+
+        let unzipProcess = Process()
+        unzipProcess.launchPath = "/usr/bin/unzip"
+        unzipProcess.currentDirectoryPath = fileURL.deletingLastPathComponent().path
+        unzipProcess.arguments = []
+
+        if let password = password {
+            unzipProcess.arguments?.append(contentsOf: ["-P", password])
+        }
+
+        unzipProcess.arguments?.append(overwrite ? "-o" : "-n")
+        unzipProcess.arguments?.append(fileURL.lastPathComponent)
+
+        if let destination = destination {
+            unzipProcess.arguments?.append(contentsOf: ["-d", destination.path])
+        }
+
+        unzipProcess.launch()
+        unzipProcess.waitUntilExit()
+    }
+}
