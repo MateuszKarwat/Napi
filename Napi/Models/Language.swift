30496eb7e7db3a9a580cfc6dd73e87e05ffa0fab
Mateusz Karwat <mateuszkarwat@gmail.com>
2016-12-15 18:04:32 +0100

    Implement 'Language' struct to represent languages with ISO-639 codes

---
+++ Napi/Models/Language.swift
@@ -0,0 +1,25 @@
+//
+//  Created by Mateusz Karwat on 15/12/2016.
+//  Copyright © 2016 Mateusz Karwat. All rights reserved.
+//
+
+import Foundation
+
+/// Represents a language based on ISO-639 code representation of names of languages.
+struct Language {
+
+    /// A correct ISO-639 code representation.
+    ///
+    /// - seealso: [Table of available codes](http://www.loc.gov/standards/iso639-2/php/English_list.php)
+    var isoCode: String
+
+    /// Returns full name of language in locale which directly comes from `isoCode`.
+    var name: String? {
+        return (Locale(identifier: isoCode) as NSLocale).displayName(forKey: .languageCode, value: isoCode)
+    }
+
+    /// Returns full name of language in current locale.
+    var currentLocaleName: String? {
+        return (Locale.current as NSLocale).displayName(forKey: .languageCode, value: isoCode)
+    }
+}
---
+++ NapiTests/LanguageTests.swift
@@ -0,0 +1,32 @@
+//
+//  LanguageTests.swift
+//  Napi
+//
+//  Created by Mateusz Karwat on 15/12/2016.
+//  Copyright © 2016 Mateusz Karwat. All rights reserved.
+//
+
+import XCTest
+@testable import Napi
+
+class LanguageTests: XCTestCase {
+
+    func testName() {
+        var polishLanguage = Language(isoCode: "pl")
+        XCTAssertEqual(polishLanguage.name, "polski")
+
+        polishLanguage.isoCode = "pol"
+        XCTAssertEqual(polishLanguage.name, "polski")
+    }
+
+    func testCurrentLocaleName() {
+        let localizedName = (Locale.current as NSLocale).displayName(forKey: .languageCode, value: "pl")
+
+        var polishLanguage = Language(isoCode: "pl")
+        XCTAssertEqual(polishLanguage.currentLocaleName, localizedName)
+
+        polishLanguage.isoCode = "pol"
+        XCTAssertEqual(polishLanguage.currentLocaleName, localizedName)
+    }
+
+}
