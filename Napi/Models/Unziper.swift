4942911052ae4a30a0d1b82ce70ce754f268b20b
Mateusz Karwat <mateuszkarwat@gmail.com>
2017-01-26 17:17:22 +0100

    Add external label for 'destination' parameter in 'Unziper' function

--- Napi/Models/Unziper.swift
+++ Napi/Models/Unziper.swift
@@ -15,7 +15,7 @@ struct Unziper {
     ///   - destination: A directory the archive should be extracted to.
     ///   - overwrite:   If it's set to `true` all exisiting files will be replaced in case of collision.
     ///   - password:    A password to the archive.
-    static func unzipFile(at fileURL: URL, destination: URL? = nil, overwrite: Bool = false, password: String? = nil) {
+    static func unzipFile(at fileURL: URL, to destination: URL? = nil, overwrite: Bool = false, password: String? = nil) {
         guard fileURL.isFile, fileURL.exists else { return }
 
         let unzipProcess = Process()
