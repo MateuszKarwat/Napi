b936a7214c4763df05bb9065d20e3990e15cd8dc
Mateusz Karwat <mateuszkarwat@gmail.com>
2017-03-05 12:17:45 +0100

    Revert "Add logging support using 'SwiftyBeaver'"
    This reverts commit 50b1e0d04a31568ec4951d0978f50839f1fae2ab.

--- Napi/AppDelegate.swift
+++ Napi/AppDelegate.swift
@@ -4,33 +4,8 @@
 //
 
 import Cocoa
-import SwiftyBeaver
-
-let log = SwiftyBeaver.self
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        setupSwiftyBeaver()
-    }
 
-    private func setupSwiftyBeaver() {
-        let console = ConsoleDestination()
-
-        let file = FileDestination()
-
-        if let cachesURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first,
-            let bundleName = Bundle.main.bundleIdentifier {
-                let logURL = cachesURL
-                    .appendingPathComponent(bundleName, isDirectory: true)
-                    .appendingPathComponent("Napi.log")
-                file.logFileURL = logURL
-        }
-
-        file.format = "$Dyyyy-MM-dd HH:mm:ss.SSS$d $C$L$c - $M"
-        _ = file.deleteLogFile()
-
-        log.addDestination(console)
-        log.addDestination(file)
-    }
 }
