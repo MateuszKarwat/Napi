0c659c2268726b2d8069b395c98ef6c0527a2dcd
Mateusz Karwat <mateuszkarwat@gmail.com>
2017-03-08 18:15:03 +0100

    Add 'DefaultPreferences' file and method to 'UserDefaults' to init them

---
+++ Napi/Models/Extensions/UserDefaults+RegisterDefaults.swift
@@ -0,0 +1,27 @@
+//
+//  Created by Mateusz Karwat on 08/03/2017.
+//  Copyright © 2017 Mateusz Karwat. All rights reserved.
+//
+
+import Foundation
+
+extension UserDefaults {
+
+    /// Tries to add the contents of the `DefaultSettings.plist` to the registration domain.
+    func registerDefaultSettings() {
+        guard
+            let defaultSettingsURL = Bundle.main.url(forResource: "DefaultPreferences", withExtension: "plist"),
+            let plistContent = FileManager.default.contents(atPath: defaultSettingsURL.path) else {
+                return
+        }
+        do {
+            let plistData = try PropertyListSerialization.propertyList(from: plistContent, options: [], format: nil) as? [String : Any]
+
+            if let plistData = plistData {
+                self.register(defaults: plistData)
+            }
+        } catch {
+            return
+        }
+    }
+}
