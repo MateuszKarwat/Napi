4cb6dd69b0c8b45533b53cae1a7aed47e1c61c89
Mateusz Karwat <mateuszkarwat@gmail.com>
2017-04-09 20:28:11 +0200

    Add custom classes for Preferences views

---
+++ Napi/Views/Preferences/Tabs/MatchPreferencesViewController.swift
@@ -0,0 +1,10 @@
+//
+//  Created by Mateusz Karwat on 09/04/2017.
+//  Copyright © 2017 Mateusz Karwat. All rights reserved.
+//
+
+import AppKit
+
+final class MatchPreferencesViewController: NSViewController {
+
+}
