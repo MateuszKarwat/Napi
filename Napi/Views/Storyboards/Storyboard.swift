8f4c51d860e909cd84b817d8df0b3276b8ee7138
Mateusz Karwat <mateuszkarwat@gmail.com>
2017-05-17 20:02:45 +0200

    Add comments to 'Storyboard' enum

--- Napi/Views/Storyboards/Storyboard.swift
+++ Napi/Views/Storyboards/Storyboard.swift
@@ -5,14 +5,25 @@
 
 import AppKit
 
+/// Enum which stores names of all storyboards in a project.
+/// This enum allows for easy access and allocation of view controllers
+/// and windows.
 enum Storyboard: String {
     case Main
     case Preferences
     case Selection
     case Progress
+}
 
+extension Storyboard {
+
+    /// Controllers in Interface Builder have an optional identifier within the storyboard. 
+    /// That identifier is set from the inspector in Interface Builder. 
+    /// The identifiers are optional, meaning that the developer only has to assign an identifier to a controller 
+    /// if they want to be able to manually invoke this method from code.
     func instantiate<C: StoryboardIdentifiable>(_ viewController: C.Type, inBundle bundle: Bundle = .main) -> C {
-        guard let vc = NSStoryboard(name: self.rawValue, bundle: bundle)
+        guard
+            let vc = NSStoryboard(name: self.rawValue, bundle: bundle)
             .instantiateController(withIdentifier: C.storyboardIdentifier) as? C
         else {
             fatalError("Couldn't instantiate \(C.storyboardIdentifier) from \(self.rawValue)")
@@ -24,17 +35,21 @@ enum Storyboard: String {
 
 // MARK: - StoryboardIdentifiable
 
+/// Adds `storyboardIdentifier` property to generate a default
+/// identifiers based on its type. In other words, if a view controller
+/// has `MyViewController` class in Storyboard associated with,
+/// it will also have a default identifier equal to "MyViewController".
 protocol StoryboardIdentifiable {
-    static var storyboardIdentifier: String { get }
     var storyboardIdentifier: String { get }
+    static var storyboardIdentifier: String { get }
 }
 
 extension StoryboardIdentifiable {
-    static var storyboardIdentifier: String {
+    var storyboardIdentifier: String {
         return String(reflecting: self).components(separatedBy: ".").dropFirst().joined(separator: ".")
     }
 
-    var storyboardIdentifier: String {
+    static var storyboardIdentifier: String {
         return String(reflecting: self).components(separatedBy: ".").dropFirst().joined(separator: ".")
     }
 
@@ -45,10 +60,6 @@ extension StoryboardIdentifiable {
 
 // MARK: - Standard Extensions
 
-extension NSViewController: StoryboardIdentifiable {
+extension NSViewController: StoryboardIdentifiable { }
 
-}
-
-extension NSWindowController: StoryboardIdentifiable {
-    
-}
+extension NSWindowController: StoryboardIdentifiable { }
