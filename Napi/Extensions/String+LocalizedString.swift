2e5ab4a01d56e5905d7ad6eb58c247f011484c9d
Mateusz Karwat <mateuszkarwat@gmail.com>
2017-06-11 19:55:00 +0200

    Add 'localizedString' extension on 'String'

---
+++ Napi/Extensions/String+LocalizedString.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Mateusz Karwat on 11/06/2017.
+//  Copyright © 2017 Mateusz Karwat. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var localizedString: String {
+        return NSLocalizedString(self, comment: "")
+    }
+}
